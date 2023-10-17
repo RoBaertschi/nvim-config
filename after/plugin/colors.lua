04326dc43099a8c65611a88fe9b76bd75f8296bf
RoBaertschi <rtmbaertschi007@gmail.com>
2023-10-17 21:47:25 +0200

    Some stuff update

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function Color(color)
-	color = color or "rose-pine"
+	color = color or "rose-pine-moon"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -7,8 +7,8 @@ function Color(color)
 
 	if vim.fn.exists("g:neovide") then
 		vim.g.neovide_transparency = 0.8
-	  vim.o.guifont = "JetBrainsMonoNL NF"
-  end
+		vim.o.guifont = "JetBrainsMonoNL NF"
+	end
 end
 
 Color()
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,12 +3,22 @@ require("neodev")
 require("neodev").setup()
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
+lsp.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({ buffer = bufnr, preserve_mappings = false })
+end)
+lsp.set_server_config({
+	on_init = function(client)
+		client.server_capabilities.semanticTokensProvider = nil
+	end,
+})
+-- lsp.preset("recommended")
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "rust_analyzer", "eslint", "lua_ls" },
-  handlers = { lsp.default_setup },
+	ensure_installed = { "tsserver", "rust_analyzer", "eslint", "lua_ls" },
+	handlers = { lsp.default_setup },
 })
 
 require("mason-null-ls").setup({
@@ -19,71 +29,69 @@ require("mason-null-ls").setup({
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
-  -- you can reuse a shared lspconfig on_attach callback here
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-          vim.lsp.buf.format({ async = false })
-          --q-§vim.lsp.buf.formatting_sync()
-        end,
-      })
-    end
-  end,
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+					vim.lsp.buf.format({ async = false })
+					--q-§vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 })
 
 require("cmp").setup({
-  sources = {
-    { name = "nvim_lua" },
-  },
+	sources = {
+		{ name = "nvim_lua" },
+	},
 })
 
 local luasnip = require("luasnip")
 
 local cmp = require("cmp")
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
-    ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
+		["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
+		-- C-b (back) C-f (forward) for snippet placeholder navigation.
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	},
 })
-
-lsp.setup()
