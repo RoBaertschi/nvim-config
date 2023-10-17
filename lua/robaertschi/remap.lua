bd67217f83131f7dbbd0223b257ac7f611264f04
RoBaertschi <rtmbaertschi007@gmail.com>
2023-10-17 22:02:19 +0200

    added remap for exiting terminal

--- lua/robaertschi/remap.lua
+++ lua/robaertschi/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", function()
 	vim.cmd.Ex()
 end)
 
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
